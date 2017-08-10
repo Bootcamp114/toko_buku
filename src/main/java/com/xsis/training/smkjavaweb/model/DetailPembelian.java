77e1f7cc9152315ff40054526536d898dad48ad6
RianAdiPrasetyo <RianAdiPrasetyo@10.8.106.37>
2017-08-10 10:59:32 +0700

    detaill pembelian

--- src/main/java/com/xsis/training/smkjavaweb/model/DetailPembelian.java
+++ src/main/java/com/xsis/training/smkjavaweb/model/DetailPembelian.java
@@ -1,13 +1,20 @@
 package com.xsis.training.smkjavaweb.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 @Entity
 @Table(name="tbl_detail_pembelian")
+@JsonIdentityInfo(generator=ObjectIdGenerators.IntSequenceGenerator.class, property="@id")
 public class DetailPembelian {
 	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	private int id;
 	private String no_faktur;
 	private String kode_buku;
