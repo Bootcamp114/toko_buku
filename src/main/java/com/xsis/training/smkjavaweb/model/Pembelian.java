a59de1092cb6184784270286a1977739d25201a4
RianAdiPrasetyo <RianAdiPrasetyo@10.8.106.37>
2017-08-10 13:22:18 +0700

    pembeliiaan

--- src/main/java/com/xsis/training/smkjavaweb/model/Pembelian.java
+++ src/main/java/com/xsis/training/smkjavaweb/model/Pembelian.java
@@ -1,10 +1,9 @@
 package com.xsis.training.smkjavaweb.model;
 
-<<<<<<< HEAD
-public class Pembelian {
-
-=======
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
@@ -14,11 +13,13 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 @Table(name="tbl_pembelian")
 @JsonIdentityInfo(generator=ObjectIdGenerators.IntSequenceGenerator.class, property="@id")
 public class Pembelian {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	private int id;
 	private String no_faktur;
 	private String diskon;
 	private Double bayar;
 	private Double kembalian;
 	private String tanggal;
->>>>>>> branch 'master' of https://github.com/Bootcamp114/toko_buku.git
+
 }
